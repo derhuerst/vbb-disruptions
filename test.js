c2ffd26d6ea188b8196f1d622c0a4d3ba0272648
Jannis R <mail@jannisr.de>
2016-03-30 03:13:40 +0200

    tests :white_check_mark:

---
+++ test.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+'use strict'
+
+const a           = require('assert')
+
+const disruptions = require('./index.js')
+
+
+
+disruptions().then((all) => {
+	a.ok('object' === typeof all, 'does not resolve with an object')
+	for (let line in all) {
+		let disruptions = all[line]
+		for (let disruption in disruptions) {
+			a.strictEqual(typeof disruption.id,          'string')
+			a.strictEqual(typeof disruption.type,        'string')
+			a.strictEqual(typeof disruption.updated,     'object')
+			a.strictEqual(typeof disruption.message,     'string')
+			a.strictEqual(typeof disruption.disturbance, 'string')
+		}
+	}
+})
