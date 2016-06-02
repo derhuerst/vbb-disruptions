e7b057e2b0e193811fdc63d3a799f68c966eee23
Jannis R <mail@jannisr.de>
2016-06-02 18:43:21 +0200

    adapt tests :white_check_mark:

--- test.js
+++ test.js
@@ -2,21 +2,29 @@
 'use strict'
 
 const a = require('assert')
+const parse = require('vbb-parse-line')
 const disruptions = require('./index.js')
 
 
 
-disruptions().catch(a.ifError)
+const err = (err) => {
+	console.error(err.stack)
+	process.exit(1)
+	return err
+}
+
+disruptions()
 .then((all) => {
-	a.ok('object' === typeof all, 'does not resolve with an object')
-	for (let line in all) {
-		let disruptions = all[line]
-		for (let disruption in disruptions) {
-			a.strictEqual(typeof disruption.id,          'string')
-			a.strictEqual(typeof disruption.type,        'string')
-			a.strictEqual(typeof disruption.updated,     'object')
-			a.strictEqual(typeof disruption.message,     'string')
-			a.strictEqual(typeof disruption.disturbance, 'string')
+	a.ok(Array.isArray(all))
+	a.ok(all.length > 0)
+	for (let d of all) {
+		a.ok(Array.isArray(d.lines))
+		for (let line of d.lines) {
+			a.strictEqual(typeof line, 'string')
+			a.ok(parse(line))
 		}
+		a.strictEqual(typeof d.where, 'string')
+		a.strictEqual(typeof d.when, 'string')
+		a.strictEqual(typeof d.description, 'string')
 	}
-})
+}, err).catch(err)
