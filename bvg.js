ee5c66fd8ee2d3fd6ab07dcd79f8e4e88a8f24b5
Jannis R <mail@jannisr.de>
2016-06-02 18:09:08 +0200

    expose functions

--- bvg.js
+++ bvg.js
@@ -13,38 +13,41 @@ const fromClass = (c) => {
 	return s ? s[1] : null
 }
 
-got(`http://www.bvg.de/de/Fahrinfo/Verkehrsmeldungen`)
-.then((res) => {
-	const $ = cheerio.load(res.body)
-	const columns = Array.from($('table.traffic-overview th'))
-		.map((e) => $(e).text())
+const disruptions = () =>
+	got(`http://www.bvg.de/de/Fahrinfo/Verkehrsmeldungen`)
+	.then((res) => {
+		const $ = cheerio.load(res.body)
+		const columns = Array.from($('table.traffic-overview th'))
+			.map((e) => $(e).text())
 
-	const row = (e) =>
-		Array.from($('td', e))
-		.reduce((disruption, cell, i) => {
-			cell = $(cell)
-			let key = columns[i]
-			let value = cell.text()
+		const row = (e) =>
+			Array.from($('td', e))
+			.reduce((disruption, cell, i) => {
+				cell = $(cell)
+				let key = columns[i]
+				let value = cell.text()
 
-			if (key === 'Linie') {
-				key = 'lines'
-				value = _.line(value)
-				if (!value) value = // wat
-					fromClass(cell.children().first().attr('class'))
-				value = [value]
-			} else if (key === 'Gültig') {
-				key = 'when'
-				value = _.clean(value)
-			} else if (key === 'Ursache und Auswirkung') key = 'description'
-			else if (key === 'Bereich') {
-				key = 'where'
-				value = _.clean(value)
-			}
+				if (key === 'Linie') {
+					key = 'lines'
+					value = _.line(value)
+					if (!value) value = // wat
+						fromClass(cell.children().first().attr('class'))
+					value = [value]
+				} else if (key === 'Gültig') {
+					key = 'when'
+					value = _.clean(value)
+				} else if (key === 'Ursache und Auswirkung') key = 'description'
+				else if (key === 'Bereich') {
+					key = 'where'
+					value = _.clean(value)
+				}
 
-			disruption[key] = value
-			return disruption
-		}, {})
+				disruption[key] = value
+				return disruption
+			}, {})
 
-	return Array.from($('table.traffic-overview tbody tr')).map(row)
-}
-, (err) => err)
+		return Array.from($('table.traffic-overview tbody tr')).map(row)
+	}
+	, (err) => err)
+
+module.exports = disruptions
--- sbahn.js
+++ sbahn.js
@@ -6,32 +6,35 @@ const _ = require('./helpers')
 
 
 
-got(`http://mobil.s-bahn-berlin.de/constructions/overview`)
-.then((res) => {
-	const $ = cheerio.load(res.body)
+const disruptions = () =>
+	got(`http://mobil.s-bahn-berlin.de/constructions/overview`)
+	.then((res) => {
+		const $ = cheerio.load(res.body)
 
-	const row = (disruption, row) => {
-		let key = $('th', row).text()
-		let value = $('td', row).text()
-		if (key === 'Linien') {
-			key = 'lines'
-			value = _.clean(value).split('\n')
+		const row = (disruption, row) => {
+			let key = $('th', row).text()
+			let value = $('td', row).text()
+			if (key === 'Linien') {
+				key = 'lines'
+				value = _.clean(value).split('\n')
+			}
+			else if (key === 'Wann') key = 'when'
+			else if (key === 'Infos') {
+				key = 'description'
+				value = _.clean(value)
+			}
+			disruption[key] = value
+			return disruption
 		}
-		else if (key === 'Wann') key = 'when'
-		else if (key === 'Infos') {
-			key = 'description'
-			value = _.clean(value)
-		}
-		disruption[key] = value
-		return disruption
+
+		return Array.from($('#content .list-group li')).map((e) => {
+			const disruption = {
+				where: $('h4', e).text()
+			}
+			Array.from($('tr', e)).reduce(row, disruption)
+			return disruption
+		})
 	}
+	, (err) => err)
 
-	return Array.from($('#content .list-group li')).map((e) => {
-		const disruption = {
-			where: $('h4', e).text()
-		}
-		Array.from($('tr', e)).reduce(row, disruption)
-		return disruption
-	})
-}
-, (err) => err)
+module.exports = disruptions
